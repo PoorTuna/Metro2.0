e88646e640d7a6132865400865a1e7b1823004b4
Oren <>
2021-04-08 12:14:53 +0000

    -Added multichat support FINISHED! -Combined socket namespaces into one function and variable. -Cleaned javascript in index page

--- Metro/routes.py
+++ Metro/routes.py
@@ -163,55 +163,71 @@ def show_chat(num):
 def something(name):
 	return render_template("error/404.html", url = name)
 
+# Socket IO connect handler
 @socketio.on('connect')
 def handle_connect():
 	print(f"{flask_login.current_user} : {flask_login.current_user.username} has connected with session id {request.sid}")
 	flask_login.current_user._session_id = request.sid
 	db.session.commit()
-	session['chat_id'] = "general"
+	session['chat_id'] = 'general'
 	join_room(session['chat_id'])
 
+# Socket IO disconnect handler
 @socketio.on('disconnect')
 def handle_disconnect():
 	print(f"{flask_login.current_user} : {flask_login.current_user.username} has disconnected.")
 	flask_login.current_user._session_id = None
 	db.session.commit()
-
+	# Change back to general chat after user disconnects
+	leave_room(session['chat_id'])
+	session['chat_id'] = "general"
+	join_room(session['chat_id'])
+			
+# Socket IO recive handler
 @socketio.on("message")
 def handle_message(msg):
-	if msg:
+	print("dxd")
+	refined_msg = msg.split(" ")
+
+	# Command Structure
+	if refined_msg[0][0] == "/":
+		# Whisper System
+		if len(refined_msg) >= 3:
+			
+			if refined_msg[0] == "/w":
+				message = msg[len(refined_msg[0]) + len(refined_msg[1]) + 2:]
+
+				recipient = metro_user.query.filter_by(username = refined_msg[1]).first()
+				if recipient:
+					if recipient._session_id:
+						emit("private_message", f"{flask_login.current_user.username} : {message}", room=recipient._session_id)
+						emit("private_message", f"To {recipient.username} : {message}", room=flask_login.current_user._session_id)
+					else:
+						emit('private_message', f"User : {recipient.username} is not online!")
+				else:
+					emit('private_message', f"User : {refined_msg[1]} does not exist!")
+		else:
+			emit('private_message', f"Invalid Command {refined_msg[0]}")
+
+	# Normal Messages:
+	elif msg:
 		if flask_login.current_user.is_authenticated:
 			emit("message", f"{flask_login.current_user.username} : {msg}", room=session['chat_id'])
 		else:
 			emit("message", f"Anonymous : {msg}", room=session['chat_id'])
-	
-	print(f" MESSAGE : {session['chat_id']}")
 
-# Private namespace test
-
-@socketio.on('join_private', namespace="/private_chat")
+# Socket IO change chat handler
+@socketio.on('join_private')
 def recv_private_chatname(cid):
-	if curr_chat := metro_chat.query.filter_by(string_id = cid).first():
-		if flask_login.current_user in curr_chat.chat_backref:
-			leave_room(session['chat_id'])
-			session['chat_id'] = cid
-			join_room(session['chat_id'])
-			print(rooms(request.sid))
-			
-# Whisper system:
-@socketio.on('private_message', namespace = "/private_chat")
-def recv_private_message(msg):
-	refined_msg = msg.split(" ")
-	if len(refined_msg) >= 3:
-		if refined_msg[0] == "/w":
-			message = msg[len(refined_msg[0]) + len(refined_msg[1]) + 2:]
+	if cid != "general":
+		if curr_chat := metro_chat.query.filter_by(string_id = cid).first():
+			if flask_login.current_user in curr_chat.chat_backref:
+				leave_room(session['chat_id'])
+				session['chat_id'] = cid
+				join_room(session['chat_id'])
 
-			recipient = metro_user.query.filter_by(username = refined_msg[1]).first()
-			if recipient:
-				if recipient._session_id:
-					emit("private_message", f"{flask_login.current_user.username} : {message}", room=recipient._session_id)
-					emit("private_message", f"To {recipient.username} : {message}", room=flask_login.current_user._session_id)
-				else:
-					emit('private_message', f"User : {recipient.username} is not online!")
-			else:
-				emit('private_message', f"User : {refined_msg[1]} does not exist!")
+	else:
+		leave_room(session['chat_id'])
+		session['chat_id'] = "general"
+		join_room(session['chat_id'])
+			
